80d436e32203b0ca1c8c4001fa6f72af640c5ede
Oziel Silva <oziel@Oziels-MacBook-Air-2.local>
2023-01-24 14:39:33 -0300

    chore: add wsgi to serve app

--- hello.py
+++ hello.py
@@ -1,14 +1,11 @@
 from flask import Flask
+from waitress import serve
 import requests
 import json
 import math
 import re
 
 app = Flask(__name__)
-
-
-
-
 
 @app.route("/")
 def get_results():
@@ -72,3 +69,7 @@ def get_results():
             else:
                 continue
     return final_results
+
+if __name__ == "__main__":
+    
+    serve(app, host="0.0.0.0", port=8080)
